d70f9fcc68ee5a4e28e4fedc07c35a9646690de4
BrianAnderson3 <brian.anderson3@target.com>
2020-07-28 11:48:18 -0500

    Create Add Log action and reducer.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types'
+import { GET_LOGS, ADD_LOG, SET_LOADING, LOGS_ERROR } from './types'
 
 export const getLogs = () => async dispatch => {
   try {
@@ -13,6 +13,29 @@ export const getLogs = () => async dispatch => {
     dispatch({
       type: LOGS_ERROR,
       payload: err.response.data,
+    })
+  }
+}
+
+export const addLog = log => async dispatch => {
+  try {
+    setLoading()
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    const data = await res.json()
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
     })
   }
 }
